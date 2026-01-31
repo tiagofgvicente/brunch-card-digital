c532edd07af4712f3e913da7ab6c4a5ae39209f4
nangelpt <tiagofgvicente@gmail.com>
2026-01-31 19:34:42 +0000

    New handler on main and changed card.html

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -70,6 +70,10 @@ func main() {
 	// Serve ficheiros estáticos da pasta /web
 	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
 
+	http.HandleFunc("/card", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "web/card.html")
+	})
+
 	fmt.Println("Server starting on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
